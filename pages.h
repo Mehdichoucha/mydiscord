c7993b4b85b79012ad42719dd4c1f08e5a3b2986
nadjla-mohamed-dini <nadjla.mohamed-dini@laplateforme.io>
2025-04-24 13:45:37 +0200

    adding the header file
    all the header files of our project stock here

---
+++ pages.h
@@ -0,0 +1,8 @@
+#ifndef PAGES_H
+#define PAGES_H
+
+#include <gtk/gtk.h>
+
+void show_testformuulaire(GtkWidget *button, gpointer user_data);
+void show_main_page(GtkWidget *button, gpointer user_data);
+#endif
